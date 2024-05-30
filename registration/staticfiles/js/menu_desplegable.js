def613f1aa4674ad847fcd42a8761865f05943b1
keiberparedes <sotomarcelinopastor.com>
2024-05-30 21:07:00 +0200

    nuevo menu desplegable sin estilos

---
+++ registration/staticfiles/js/menu_desplegable.js
@@ -0,0 +1,38 @@
+/* FUNCION PARA EL MENU 1 */
+const menuButton = document.getElementById('menuButton');
+const submenu = document.getElementById('submenu');
+
+menuButton.addEventListener('click', function() {
+  if (submenu.style.display === 'none') {
+    submenu.style.display = 'block';
+  } else {
+    submenu.style.display = 'none';
+  }
+});
+
+/* FUNCION PARA EL MENU 2 */
+const menuButton2 = document.getElementById('menuButton2');
+const submenu2 = document.getElementById('submenu2');
+
+menuButton2.addEventListener('click', function() {
+  if (submenu2.style.display === 'none') {
+    submenu2.style.display = 'block';
+  } else {
+    submenu2.style.display = 'none';
+  }
+});
+
+
+/* FUNCION PARA EL MENU 3 */
+const menuButton3 = document.getElementById('menuButton3');
+const submenu3 = document.getElementById('submenu3');
+
+menuButton3.addEventListener('click', function() {
+  if (submenu3.style.display === 'none') {
+    submenu3.style.display = 'block';
+  } else {
+    submenu3.style.display = 'none';
+  }
+});
+
+
