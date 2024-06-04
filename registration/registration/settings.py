fa99643db4d3041928bc3aa0ad396dcce9f8f79b
donatello <andruthsanchez@gmail.com>
2024-06-04 16:46:09 -0400

    modificacion con la base de datos a postgres

--- registration/registration/settings.py
+++ registration/registration/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'registration.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'NAME': 'SGH-COCOROTE',
+        'USER': 'postgres',
+        'PASSWORD': '123', 
     }
 }
 
